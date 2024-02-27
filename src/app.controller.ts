0d67f1fc4103834bb273969fc41e31d6c5f3960b
domutala <33329431+domutala@users.noreply.github.com>
2024-02-27 18:07:17 +0100

    Initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/')
+  convert(
+    @Query('to') to: string,
+    @Query('from') from: string,
+    @Query('value') value: string,
+    @Query('base') base: string,
+  ) {
+    return this.appService.convert(Number(value), from, to);
+  }
+
+  @Get('/rate')
+  rate(@Query('base') base: string) {
+    return this.appService.rate(base);
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { IRate } from 'models';
+
+@Injectable()
+export class AppService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async convert(value: number, from: string, to: string) {
+    const rate = await this.rate();
+
+    if (from === rate.base) {
+      if (to === rate.base) {
+        return value;
+      } else if (rate.rates[to]) {
+        return value * rate.rates[to];
+      } else {
+        throw `Rate for ${to} not found.`;
+      }
+    } else if (to === rate.base) {
+      if (rate.rates[from]) {
+        return value / rate.rates[from];
+      } else {
+        throw `Rate for ${from} not found.`;
+      }
+    } else {
+      if (rate.rates[from] && rate.rates[to]) {
+        return (value / rate.rates[from]) * rate.rates[to];
+      } else {
+        throw `Rate for ${from} or ${to} not found.`;
+      }
+    }
+  }
+
+  async rate(base?: string) {
+    const response = await this.httpService
+      .get('https://cdn.taux.live/api/latest.json')
+      .toPromise();
+
+    const rate: IRate = response.data;
+    rate.base = 'USD';
+
+    if (base && rate.rates[base] && base !== rate.base) {
+      const baseRate = rate.rates[base];
+
+      for (const currency in rate.rates) {
+        rate.rates[currency] = rate.rates[currency] / baseRate;
+        rate.base = base;
+      }
+    }
+
+    return rate;
+  }
+}
